b08f33b1dfadb83ceec61839f9472e56cc602a47
Cleverson Nahum <cleversonahum@gmail.com>
2018-03-20 19:31:10 -0300

    Trying to run threads example in java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//import channels.*;
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+	    //System.out.println("Hello World!");
+        //Channel R1 = new Channel( "Thread-1");
+        //R1.start();
+      
+        //Channel R2 = new Channel( "Thread-2");
+        //R2.start();
+	    
+    }
+}
