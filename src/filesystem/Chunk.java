20991d2bb4690beb0423de18d62d8c6c688e29de
Cleverson Nahum <cleversonahum@gmail.com>
2018-03-23 22:56:06 -0300

    storeChunk function working

--- src/filesystem/Chunk.java
+++ src/filesystem/Chunk.java
@@ -1,10 +1,17 @@
 package filesystem;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.io.FileOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Chunk {
     private String fileId;
     private int chunkNumber;
     private int replicationDeg;
     private int contentSize;
+    private final String PATH = "data/chunks/";
     
     public Chunk(String id, int chunkn, int replication,  byte[] body){
         fileId = id;
@@ -18,6 +25,13 @@ public class Chunk {
     
     public void storeChunk(byte[] data){
         System.out.println("Writing Chunk into Disk"); //code line to delete after
+        try{
+            Path pathChunk = Paths.get(PATH.concat(fileId));
+            Files.createDirectories(pathChunk.getParent());
+            Files.write(pathChunk, data);
+            
+        }
+        catch(Exception e) {e.printStackTrace();} 
     }
     
     public byte[] getChunk(String id) {
