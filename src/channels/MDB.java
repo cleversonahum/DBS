9f74a253bd7d9af3beea15d603fb4219ee4e6469
bruno-ribeiro <up201505817@fe.up.pt>
2018-03-30 15:53:47 +0100

    Add files via upload

--- src/channels/MDB.java
+++ src/channels/MDB.java
@@ -2,6 +2,7 @@ package channels;
 
 import java.net.*;
 import filesystem.*;
+import utilities.*;
 import java.util.Random;
 
 public class MDB extends Channel {
@@ -10,20 +11,23 @@ public class MDB extends Channel {
 	}
 	
 	public void select (DatagramPacket dPacket) {
-	    String msg = new String(dPacket.getData()); //I believe thaimport filesystem.*;t will change, because here the bytes from the content are converted to String
-	    String[] cmd = msg.split("\\s+"); //Splitting message received
-	    
-	    if(cmd[0].trim().equals("PUTCHUNK")) { //Checks if the message has the command expected
-	       Chunk newChunk = new Chunk("ExampleID", 1, 2, ("teste").getBytes()); //Stores the chunk received into this server
+		
+		Message M=new Message(dPacket);
+		
+	   // String msg = new String(dPacket.getData()); //I believe thaimport filesystem.*;t will change, because here the bytes from the content are converted to String
+	  //  String[] cmd = msg.split("\\s+"); //Splitting message receive
+	    if(M.getMessageType().equals("PUTCHUNK")) { //Checks if the message has the command expected
+	    	   
+	       Chunk newChunk = new Chunk(M.getFile_id(), Integer.parseInt(M.getChunkNumber()), Integer.parseInt(M.getReplicationDegree()), M.getBody()); //Stores the chunk received into this server
             try {
                 Random random = new Random();
                 Thread.sleep(random.nextInt(400));
             } catch (Exception e) {e.printStackTrace();}
 
-	       sendMessage("Stored BLA BLA BLA", "224.0.0.0", 3781); //Sending Example message of confirmation into MC channel
+	       sendMessage("Stored "+ M.getVersion()+" "+M.getSender_id()+" "+M.getFile_id()+" "+M.getChunkNumber()+" "+"\r\n\r\n", "224.0.0.0", 3781); //Sending Example message of confirmation into MC channel
         }
 	    else
-	       System.out.println("Command \""+cmd[0]+"\" Not Found");
+	       System.out.println("Command \""+M.getMessageType()+"\" Not Found");
 	    
 	}
     
