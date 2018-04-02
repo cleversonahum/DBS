cbd14b57b771e30fd4a57839f1a67a28d187201e
bruno-ribeiro <up201505817@fe.up.pt>
2018-04-02 10:05:19 +0100

    Add files via upload

--- src/channels/MC.java
+++ src/channels/MC.java
@@ -3,20 +3,21 @@ package channels;
 import java.net.*;
 import filesystem.*;
 import java.util.Random;
+import utilities.Message;
 
 public class MC extends Channel {
 	public MC(char type, String address, int port, int packetS, String name) {
-		super(type, address, port, packetS, name); 
+		super(type, address, port, packetS, name);
 	}
-	
+
 	public void select (DatagramPacket dPacket) {
-	    String msg = new String(dPacket.getData());
-	    String[] cmd = msg.split("\\s+"); //Splitting message received
-	    
-	    if(cmd[0].trim().equals("DELETE")) { //Checks if the message has the command expected
+
+			Message M=new Message(dPacket);
+
+			if(M.getMessageType().equals("DELETE")) { //Checks if the message has the command expected
 	       //Here some Function to Delete the file and all chunks related to it
 	       System.out.println("Delete chunk function called");
-	       
+
             try {
                 Random random = new Random();
                 Thread.sleep(random.nextInt(400));
@@ -25,8 +26,8 @@ public class MC extends Channel {
 	       sendMessage("Removed BLA BLA BLA", "224.0.0.0", 3781); //Sending Example message of confirmation into MC channel
         }
 	    else
-	       System.out.println("Command \""+cmd[0]+"\" Not Found");
-	    
+	       System.out.println("Command \""+"\" Not Found");
+
 	}
-    
+
 }
