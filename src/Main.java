a62faf80b00ab4664a0664a683c8b3d95a09d5c8
bruno-ribeiro <up201505817@fe.up.pt>
2018-04-03 00:13:32 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args) {
       
         System.setProperty("java.net.preferIPv4Stack", "true");
-        Channel MC = new MC('m',"224.0.0.0",3781, 65000, "MC");
-        Channel MDB = new MDB('m',"224.0.0.1",3785, 65000, "MDB");
-        Channel MDR = new MDR('m',"224.0.0.2",3789, 512, "MDR");
+        Channel MC = new MC('m',"224.0.0.0",3781, 80000, "MC");
+        Channel MDB = new MDB('m',"224.0.0.1",3785, 80000, "MDB");
+        Channel MDR = new MDR('m',"224.0.0.2",3789, 80000, "MDR");
         UC UC = new UC();
         MC.start();
         MDB.start();
         MDR.start();
         UC.start();
         
-        //System.out.println("Hello");
-        //FileChunk.splitFile("./src/teste2",1234,2,"224.0.0.1",3785,(channels.MDB) MDB);
+        System.out.println("Hello");
+        FileChunk.deleteFile("teste2",1234,"224.0.0.0",3781,(channels.MC) MC);
     }
 }
