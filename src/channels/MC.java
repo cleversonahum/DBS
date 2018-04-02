032ae4fd21aaf95d0291ca088f2502a973432290
bruno-ribeiro <up201505817@fe.up.pt>
2018-04-03 00:24:26 +0100

    Add files via upload

--- src/channels/MC.java
+++ src/channels/MC.java
@@ -13,16 +13,16 @@ public class MC extends Channel {
 	public void select (DatagramPacket dPacket) {
 
 			Message M=new Message(dPacket);
-			System.out.println("MC MC");
 			
 			if(M.getMessageType().equals("STORED")) {
 				System.out.println("RECEIVED STORE with NUMBER: " + M.getChunkNumber());
 			}
 			
-			else if(M.getMessageType().equals("DELETE")) { //Checks if the message has the command expected
+		else if(M.getMessageType().equals("DELETE")) { //Checks if the message has the command expected
 	       //Here some Function to Delete the file and all chunks related to it
 	       System.out.println("Delete chunk function called");
-
+	       Chunk.deleteChunks(M.getFile_id());
+	      
             try {
                 Random random = new Random();
                 Thread.sleep(random.nextInt(400));
@@ -31,7 +31,7 @@ public class MC extends Channel {
 	       //sendMessage("Removed BLA BLA BLA", "224.0.0.0", 3781); //Sending Example message of confirmation into MC channel
         }
 	    else
-	       System.out.println("Command \""+"\" Not Found");
+	       System.out.println("Command \""+M.getMessageType()+"\" Not Found");
 
 	}
 
