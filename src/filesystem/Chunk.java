93c78f74b0ff029dfba9a52e28b42fab5d4e3205
bruno-ribeiro <up201505817@fe.up.pt>
2018-04-03 00:13:13 +0100

    Add files via upload

--- src/filesystem/Chunk.java
+++ src/filesystem/Chunk.java
@@ -44,4 +44,29 @@ public class Chunk {
         
         return rChunk;
     }
+    
+    /*
+     * Deletes the chunks in the respective folders
+     * @param fileName Respective file id of the chunks
+     */
+    public static void deleteChunks (String fileID) {
+
+        String filePath= "data/chunks/";
+        File file = new File(filePath);
+        File[] listDir = file.listFiles();
+
+          for (int i = 0; i < listDir.length; i++) {
+
+              if (listDir[i].isDirectory() && listDir[i].getName().contains(fileID)) {
+
+                File[] listChunks = listDir[i].listFiles();
+                 for(int j = 0; j < listChunks.length; j++){
+                            listChunks[j].delete();
+                             System.out.println("Chunk Deleted");
+                       }
+                         listDir[i].delete();
+              }
+          }
+    }
+    
 }
