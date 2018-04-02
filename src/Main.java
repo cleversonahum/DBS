ff4b9f4d7e86415e02bbfd7737cd3a4b3980e832
Cleverson Nahum <cleversonahum@gmail.com>
2018-04-02 19:04:53 -0300

    fixing size packet of MC and MDB channel

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
       
         System.setProperty("java.net.preferIPv4Stack", "true");
-        Channel MC = new MC('m',"224.0.0.0",3781, 512, "MC");
-        Channel MDB = new MDB('m',"224.0.0.1",3785, 512, "MDB");
+        Channel MC = new MC('m',"224.0.0.0",3781, 65000, "MC");
+        Channel MDB = new MDB('m',"224.0.0.1",3785, 65000, "MDB");
         Channel MDR = new MDR('m',"224.0.0.2",3789, 512, "MDR");
         UC UC = new UC();
         MC.start();
@@ -16,6 +16,6 @@ public class Main {
         UC.start();
         
         System.out.println("Hello");
-        FileChunk.splitFile("teste2",1234,2,"224.0.0.1",3785,(channels.MDB) MDB);
+        FileChunk.splitFile("./src/teste2",1234,2,"224.0.0.1",3785,(channels.MDB) MDB);
     }
 }
